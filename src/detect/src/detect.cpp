150a6ae2576505cf4e16e0092834125d18faf4e0
chae99-young <chae99@126.com>
2022-10-11 09:55:57 +0800

    cannot rosrun

--- src/detect/src/detect.cpp
+++ src/detect/src/detect.cpp
@@ -11,13 +11,20 @@
 #include <cv_bridge/cv_bridge.h>
 #include "opencv2/opencv.hpp"
 
-
 #include "yololayer.h"
 #include "ros/ros.h"
 #include "detect/box.h" 
 #include "detect/boxes.h"
 
 yolov5 *det;
+ros::Publisher boxes_pub;
+
+// void boxcpy(detect::box* boxes,   detect::box pub_box){
+//             boxes->x = pub_box.x;
+//             boxes->y = pub_box.y ;
+//             boxes->confidence = pub_box.confidence;
+//     return;
+// }
 
 void image_callback(const sensor_msgs::Image &msg){
     //int w = img->width;
@@ -32,35 +39,42 @@ void image_callback(const sensor_msgs::Image &msg){
     cudaMemcpy(d_image, img.data, w * h * 3 * sizeof(unsigned char),cudaMemcpyHostToDevice);
 
     std::vector<Yolo::Detection> &res = det->detect(d_image, w, h,img);
-    detect::boxes pub_boxes;
-    pub_boxes = new detect::box[res.size()]
+  //detect::boxes *pub_boxes = new detect::boxes ;
+   detect::boxes pub_boxes;
+//    detect::box *pb = new detect::box[res.size()];
+   // cudaMalloc((detect::box *)pub_boxes->boxes,sizeof(detect::box)*res.size())
     pub_boxes.box_num = res.size();
     for (size_t j = 0; j < res.size(); j++)
-        {   
+        {               
             detect::box pub_box;
             pub_box.x = res[j].bbox[0];
             pub_box.y = res[j].bbox[1];
             pub_box.confidence = res[j].conf;
-            pub_boxes.boxes[j]=pub_box;
+            // pub_boxes->boxes[j]=pub_box;
+            //boxcpy(&pub_boxes.boxes[j],pub_box);
+            pub_boxes.boxes.push_back(pub_box);
         }
-    for(int i=0;i<res.size();i++)
-        print(pub_boxes[i]);
+    //for(int i=0;i<res.size();i++)
+     //   print(pub_boxes.boxes[i]);
     boxes_pub.publish(pub_boxes);
+    // delete(pb);
+    //delete(pub_boxes);
     cudaFree(d_image);
 }
 
 int main(int argc, char **argv)
 {
+
     ros::init(argc,argv,"detect");
     ros::NodeHandle n;
-
+    
     cudaSetDevice(0);
 
-    std::string engine_name = "/home/weilan/watcher/src/detect/engine/yolov5_best.engine";
-    *det = new yolov5(engine_name, 4); //engine_name,class_number
+    std::string engine_name = "/home/tuf2/watcher/src/detect/engine/yolov5_best.engine";
+    det = new yolov5(engine_name, 4); //engine_name,class_number
 
     ros::Subscriber sub = n.subscribe("/watcher/image_raw",1,image_callback);
-    ros::Publisher boxes_pub = n.advertise<detect::boxes>("/vehicles_boxes",1);
+    boxes_pub = n.advertise<detect::boxes>("/vehicles_boxes",1);
     ros::Rate loop_rate(10);
     
     //img = cv::imread("/home/weilan/watcher/src/detect/msg/bus.jpg");
